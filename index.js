cc75c61187800326e5da7c2a750166f7211e50e0
RavemanThc <vlad.garkusha1999@gmail.com>
2026-02-22 20:08:22 +0000

    Deploying to gh-pages from @ RavemanThc/Kino_Afisha@3fba086673616c02cb53eb29df5c2c3e60ed373a 🚀

--- index.js
+++
@@ -1,2 +0,0 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const o=document.querySelector(".scroll-top");window.addEventListener("scroll",()=>{window.scrollY>400?o.classList.add("show"):o.classList.remove("show")});o.addEventListener("click",()=>{window.scrollTo({top:0,behavior:"smooth"})});
-//# sourceMappingURL=index.js.map
